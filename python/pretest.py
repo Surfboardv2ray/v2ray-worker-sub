22141bf878242e51813acf556d094c1694644d82
SurfboardV2ray <125461738+Surfboardv2ray@users.noreply.github.com>
2024-04-09 04:46:02 -0400

    Update pretest.py

--- python/pretest.py
+++ python/pretest.py
@@ -3,5 +3,5 @@ with open('sub', 'r') as f:
 
 selected_lines = [line for line in lines if line.startswith(('vmess://', 'ss://', 'ssr://', 'trojan://'))]
 
-with open('python/speedtest_input', 'w') as f:
+with open('python/pretest', 'w') as f:
     f.writelines(selected_lines)
