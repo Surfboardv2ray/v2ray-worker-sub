3a1b4e500ad5d61b29227d1a6ce77599a0132790
SurfboardV2ray <125461738+Surfboardv2ray@users.noreply.github.com>
2024-04-09 04:11:39 -0400

    Update replace.py

--- python/replace.py
+++ python/replace.py
@@ -1,3 +1,5 @@
+import re
+
 with open('sub', 'r') as f:
     lines = f.readlines()
 
@@ -5,6 +7,8 @@ new_lines = []
 for line in lines:
     if line.startswith('://'):
         line = 'vmess' + line
+    line = re.sub(r'\bauto://\b', 'vmess://', line)
+    line = re.sub(r'\bnone://\b', 'vmess://', line)
     new_lines.append(line)
 
 with open('sub', 'w') as f:
